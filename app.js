f300379010aaec41714b1f6b541700885f1287e5
Henry <henydoudoc@gmail.com>
2023-11-20 18:47:19 +0100

    Actualizacion

--- app.js
+++ app.js
@@ -1,143 +1,263 @@
-const express = require('express')
-const url = require('url')
-const { v4: uuidv4 } = require('uuid')
-const database = require('./utilsMySQL.js')
-const app = express()
-const port = 3000
+const express = require("express");
+const url = require("url");
+const { v4: uuidv4 } = require("uuid");
+const database = require("./utilsMySQL.js");
+const app = express();
+const port = 3007;
 
 // Crear i configurar l'objecte de la base de dades
-var db = new database()
+var db = new database();
 db.init({
-host: "localhost",
-port: 3307,
-user: "root",
-password: "pwd",
-database: "world"
-})
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "pwd",
+  database: "world"
+  })
 
-// Publicar arxius carpeta ‘public’
-app.use(express.static('public'))
+
+// Publicar arxius carpeta 'public'
+app.use(express.static("public"));
 
 // Configurar per rebre dades POST en format JSON
 app.use(express.json());
 
 // Configurar direcció '/testDB'
-app.get('/', testDB)
-async function testDB (req, res) {
-let rst = await db.query('SELECT * FROM usuarios')
-res.send(rst)
+app.get("/", testDB);
+async function testDB(req, res) {
+  let rst = await db.query("SELECT * FROM usuarios");
+  res.send(rst);
 }
 
 // Configurar la direcció '/ajaxCall'
-app.post('/ajaxCall', ajaxCall)
-async function ajaxCall (req, res) {
-    let objPost = req.body;
-    let result = ""
+app.post("/ajaxCall", ajaxCall);
+async function ajaxCall(req, res) {
+  let objPost = req.body;
+  let result = "";
 
-    // Simulate delay (1 second)
-    await new Promise(resolve => setTimeout(resolve, 1000));
+  // Simulate delay (1 second)
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    if (objPost.callType == 'tableData') {
-        try {
-            let qry = `SELECT * FROM ${objPost.table}`
-            result = await db.query(qry)
-        } catch (e) {
-            console.error('Error at "ajaxCall":', e)
-        }
+  if (objPost.callType == "tableData") {
+    try {
+      let qry = `SELECT * FROM ${objPost.table}`;
+      result = await db.query(qry);
+    } catch (e) {
+      console.error('Error at "ajaxCall":', e);
     }
-    res.send(result)
+  }
+  res.send(result);
 }
 
 // Configurar la direcció '/getTables'
-app.get('/getTables', (req, res) => {
-    db.query('SHOW TABLES', (err, result) => {
-      if (err) {
-        console.error('Error al obtener las tablas:', err);
-        res.status(500).send('Error interno del servidor');
-        return;
-      }
-  
-      const tables = result.map(table => table.Tables_in_usuarios);
-      res.json(tables);
-    });
+app.get("/getTables", (req, res) => {
+  db.query("SHOW TABLES", (err, result) => {
+    if (err) {
+      console.error("Error al obtener las tablas:", err);
+      res.status(500).send("Error interno del servidor");
+      return;
+    }
+
+    const tables = result.map((table) => table.Tables_in_usuarios);
+    res.json(tables);
+  });
 });
 
-// app.js
+app.post("/addData", addData);
 
-// ... (tu código existente)
-
-app.post('/addData', addData);
 async function addData(req, res) {
-    const { nom, mail } = req.body;
+  const { nom, mail } = req.body;
 
-    try {
-        // Ejemplo de inserción en una base de datos MySQL
-        const sql = `INSERT INTO usuarios (nom, mail) VALUES ('${nom}','${mail}')`;
+  try {
+    // Verificar si la tabla está vacía antes de reiniciar AUTO_INCREMENT
+    const checkEmptyTable = `SELECT COUNT(*) AS rowCount FROM usuarios`;
+    const result = await db.query(checkEmptyTable);
+    const rowCount = result[0].rowCount;
 
-        // Objeto con las variables y sus valores correspondientes
-        const values = { $nom: nom, $mail: mail };
-
-        await db.query(sql, values);
-        res.json({ success: true, message: 'Datos añadidos con éxito' });
-    } catch (error) {
-        console.error('Error al agregar datos:', error);
-        res.status(500).send({ success: false, message: 'Error interno del servidor' });
+    if (rowCount === 0) {
+      // Si la tabla está vacía, reiniciar AUTO_INCREMENT
+      const resetAutoIncrement = `ALTER TABLE usuarios AUTO_INCREMENT = 1`;
+      await db.query(resetAutoIncrement);
     }
+
+    // Ejemplo de inserción en una base de datos MySQL
+    const sql = `INSERT INTO usuarios (nom, mail) VALUES ('${nom}', '${mail}')`;
+
+    // Objeto con los valores correspondientes
+    const values = { $nom: nom, $mail: mail };
+
+    await db.query(sql, values);
+    res.json({ success: true, message: "Datos añadidos con éxito" });
+  } catch (error) {
+    console.error("Error al agregar datos:", error);
+    res
+      .status(500)
+      .send({ success: false, message: "Error interno del servidor" });
+  }
 }
 
 
-
-
-// ... (tu código existente)
-
-
-app.post('/updateData', updateData);
+app.post("/updateData", updateData);
 async function updateData(req, res) {
-    const { id, nuevonom, nuevamail } = req.body;
+  const { id, nuevonom, nuevamail } = req.body;
 
-    try {
-        // Ejemplo de actualización en una base de datos MySQL
-        const sql = `UPDATE usuarios SET nom = '${nuevonom}', mail = '${nuevamail}' WHERE id = '${id}'`;
-        await db.query(sql, [nuevonom, nuevamail, id]);
+  try {
+    // Ejemplo de actualización en una base de datos MySQL
+    const sql = `UPDATE usuarios SET nom = '${nuevonom}', mail = '${nuevamail}' WHERE id = '${id}'`;
+    await db.query(sql, [nuevonom, nuevamail, id]);
 
-        res.status(200).send({ success: true, message: 'Datos modificados con éxito' });
-    } catch (error) {
-        console.error('Error al actualizar datos:', error);
-        res.status(500).send({ success: false, message: 'Error interno del servidor' });
-    }
+    res
+      .status(200)
+      .send({ success: true, message: "Datos modificados con éxito" });
+  } catch (error) {
+    console.error("Error al actualizar datos:", error);
+    res
+      .status(500)
+      .send({ success: false, message: "Error interno del servidor" });
+  }
 }
 
-app.post('/deleteData', deleteData);
+app.post("/deleteData", deleteData);
 async function deleteData(req, res) {
-    let objPost = req.body;
+   const {deleteid} = req.body;
 
-    const id = objPost.id;
 
-    const sql = `DELETE FROM usuarios WHERE id = '${id}'`;
+ 
 
-    await db.query(sql, [id], (err, result) => {
-        if (err) {
-            console.error('Error al borrar datos:', err);
-            return res.status(500).send('Error interno del servidor');
-        }
+  try {
+    // Ejemplo de inserción en una base de datos MySQL
+    const sql = `DELETE FROM usuarios WHERE id = ${deleteid} `;
 
-        res.send(result);
-    });
+    // Objeto con las variables y sus valores correspondientes
+
+    await db.query(sql, [deleteid]);
+    res.json({ success: true, message: "Datos añadidos con éxito" });
+  } catch (error) {
+    console.error("Error al agregar datos:", error);
+    res
+      .status(500)
+      .send({ success: false, message: "Error interno del servidor" });
+  }
 }
+
+app.post("/createTable", createTable);
+
+async function createTable(req, res) {
+  const { tableName, columns } = req.body;
+
+  try {
+    if (!tableName || !columns || columns.length === 0) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "Datos insuficientes para crear la tabla",
+        });
+    }
+
+    // Validar tipos de columna permitidos (puedes personalizar esto según tus necesidades)
+    const allowedColumnTypes = ["INT", "VARCHAR(255)", "TEXT", "DATE"];
+    const isValidColumnType = columns.every((column) =>
+      allowedColumnTypes.includes(column.type.toUpperCase())
+    );
+
+    if (!isValidColumnType) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Tipo de columna no válido" });
+    }
+
+    // Escapar nombres de tabla y columnas (ajusta según la biblioteca que estés utilizando)
+    const escapedTableName = escapeIdentifier(tableName);
+    const escapedColumns = columns
+      .map((column) => `${escapeIdentifier(column.name)} ${column.type}`)
+      .join(", ");
+
+    // Ejemplo de creación de una tabla en una base de datos MySQL
+    const sql = `CREATE TABLE IF NOT EXISTS ${escapedTableName} (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            ${escapedColumns}
+        )`;
+
+    await db.query(sql);
+    res.json({ success: true, message: "Tabla creada con éxito" });
+  } catch (error) {
+    console.error("Error al crear la tabla:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Error interno del servidor" });
+  }
+}
+
+// Función de escape de identificadores (ajusta según la biblioteca que estés utilizando)
+function escapeIdentifier(identifier) {
+  // Implementa la lógica de escape aquí
+  // Por ejemplo, puedes usar backticks para MySQL
+  return `\`${identifier}\``;
+}
+
+app.post("/updateTable", updateTable);
+
+async function updateTable(req, res) {
+  const { tableName, columnName, columnType, operationSelect } = req.body; // Added columnType to capture the type of the new column
+  console.log("Consola---->" + tableName + columnName+operationSelect);
+
+  try {
+    // Ejemplo de actualización en una base de datos MySQL
+    if (operationSelect == 'add') {
+      const addColumnSQL = `ALTER TABLE ${tableName} ADD COLUMN ${columnName} ${columnType}`;
+      await db.query(addColumnSQL, [tableName, columnName, columnType]);
+    } else {
+      const dropColumnSQL = `ALTER TABLE ${tableName} DROP COLUMN ${columnName}`;
+      await db.query(dropColumnSQL, [tableName, columnName]);
+    }
+
+    // Nueva consulta para agregar la columna
+    res
+      .status(200)
+      .send({ success: true, message: "Datos modificados con éxito" });
+  } catch (error) {
+    console.error("Error al actualizar datos:", error);
+    res
+      .status(500)
+      .send({ success: false, message: "Error interno del servidor" });
+  }
+}
+app.post("/deleteTable",deleteTable);
+    async function deleteTable(req,res){
+        const {tableName} =req.body;
+
+        try {
+            // Ejemplo de inserción en una base de datos MySQL
+            const sql = `DROP TABLE ${tableName}`;
+        
+            // Objeto con las variables y sus valores correspondientes
+        
+            await db.query(sql, [tableName]);
+            res.json({ success: true, message: "Datos añadidos con éxito" });
+          } catch (error) {
+            console.error("Error al agregar datos:", error);
+            res
+              .status(500)
+              .send({ success: false, message: "Error interno del servidor" });
+          }
+        
+    }
+
 
 
 // Activar el servidor
-const httpServer = app.listen(port, appListen)
-function appListen () {
-console.log(`Example app listening on: http://localhost:${port}`)
+const httpServer = app.listen(port, appListen);
+function appListen() {
+  console.log(`Example app listening on: http://localhost:${port}`);
 }
 
 // Close connections when process is killed
-process.on('SIGTERM', shutDown);
-process.on('SIGINT', shutDown);
+process.on("SIGTERM", shutDown);
+process.on("SIGINT", shutDown);
 function shutDown() {
-console.log('Shutting down gracefully');
-httpServer.close()
-db.end()
-process.exit(0);
+  console.log("Shutting down gracefully");
+  httpServer.close();
+  db.end();
+  process.exit(0);
 }
--- public/script.js
+++ public/script.js
@@ -3,9 +3,12 @@ async function init() {
     await load('usuarios')
 }
 
+
 async function load(tableName) {
 
+
     document.getElementById('tablaDatos').innerHTML = '';
+
 
     // Preparar l'objecte que s'envia al servidor,
     // amb la petició de dades de la taula
@@ -13,6 +16,7 @@ async function load(tableName) {
         callType: 'tableData',
         table: tableName
     }
+
 
     // Fer la petició al servidor
     let resultData = []
@@ -30,10 +34,12 @@ async function load(tableName) {
         console.error('Error at "load":', e)
     }
 
+
     let headRow = document.createElement('tr');
     let thead0 = document.createElement('td');
     let thead1 = document.createElement('td');
     let thead2 = document.createElement('td');
+
 
     thead0.textContent = "ID";
     thead1.textContent = "nom";
@@ -43,6 +49,7 @@ async function load(tableName) {
     headRow.appendChild(thead2);
     document.getElementById('tablaDatos').appendChild(headRow);
 
+
     // Mostrar les dades rebudes
     for (let row in resultData) {
         let rowValue = resultData[row]
@@ -50,6 +57,7 @@ async function load(tableName) {
         let cell0 = document.createElement('td');
         let cell1 = document.createElement('td');
         let cell2 = document.createElement('td');
+
 
         if (tableName == 'usuarios') {
             cell0.textContent = rowValue.id
@@ -63,6 +71,7 @@ async function load(tableName) {
     }
 }
 
+
 async function loadTables() {
     try {
       let result = await fetch('/getTables');
@@ -70,6 +79,7 @@ async function loadTables() {
         throw new Error(`Error HTTP: ${result.status}`);
       }
       let tables = await result.json();
+
 
       // Muestra la lista de tablas en el elemento con id 'tableList'
       let tableList = document.getElementById('tableList');
@@ -84,16 +94,19 @@ async function loadTables() {
   }
 // script.js
 
+
 async function agregarDato() {
     const nom = document.getElementById('addnom').value;
     const mail = document.getElementById('addmail').value;
     console.log('Valores:', nom, mail);
+
 
     // Crear el objeto de datos a enviar al servidor
     const requestData = {
         nom: nom,
         mail: mail
     };
+
 
     try {
         // Realizar una solicitud AJAX para agregar datos en el servidor
@@ -102,6 +115,7 @@ async function agregarDato() {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(requestData)
         });
+
 
         if (result.ok) {
             // La solicitud fue exitosa
@@ -122,11 +136,16 @@ async function agregarDato() {
 
 
 
+
+
+
+
 async function modificarDato() {
     const modifyid = document.getElementById('modifyID').value;
     const nom = document.getElementById('modifynom').value;
     const mail = document.getElementById('modifymail').value;
     console.log(modifyid, nom, mail);
+
 
     // Crear el objeto de datos a enviar al servidor
     const requestData = {
@@ -135,6 +154,7 @@ async function modificarDato() {
         nuevamail: mail
     };
 
+
     try {
         // Realizar una solicitud AJAX para modificar datos en el servidor
         let result = await fetch('/updateData', {
@@ -142,6 +162,7 @@ async function modificarDato() {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(requestData)
         });
+
 
         if (result.ok) {
             // La solicitud fue exitosa
@@ -159,37 +180,253 @@ async function modificarDato() {
     }
 }
 
+
 async function eliminarDato() {
     // Obtener confirmación del usuario
     const deleteid = document.getElementById('deleteID').value;
     const confirmacion = confirm('¿Estás seguro de que deseas borrar este registro?');
 
+
     if (!confirmacion) {
         console.log('Borrado cancelado por el usuario');
         return;
     }
-
+    const requ = {
+        deleteid:deleteid
+    }
     // Realizar la solicitud AJAX para borrar datos
     try {
         load('usuarios');
         let result = await fetch('/deleteData', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id: deleteid, confirmation: 'confirmar' })
+            body: JSON.stringify(requ)
         });
+
 
         if (result.ok) {
             // La solicitud fue exitosa
             console.log('Datos borrados con éxito');
+            document.getElementById('feedbackMessageDeleteData').textContent = 'Dato borrado con éxito';
             // Puedes realizar acciones adicionales aquí si es necesario
             // Por ejemplo, recargar la tabla de datos
         } else {
             // La solicitud falló
             console.error('Error al borrar datos:', result.status, result.statusText);
+            document.getElementById('feedbackMessageDeleteData').textContent = 'Error al borrar datos';
             // Puedes mostrar un mensaje de error al usuario o realizar acciones adicionales
         }
     } catch (error) {
         console.error('Error al procesar la solicitud:', error);
         // Puedes mostrar un mensaje de error al usuario o realizar acciones adicionales
     }
+}
+function crearCamposDeColumnas() {
+    var numColumnas = document.getElementById('numeroColumnas').value;
+    var columnInputsContainer = document.getElementById('columnInputs');
+    columnInputsContainer.innerHTML = ''; // Limpiar contenido existente
+
+
+    for (var i = 1; i <= numColumnas; i++) {
+        var label = document.createElement('label');
+        label.textContent = 'Columna' + i + ':';
+
+
+        var input = document.createElement('input');
+        input.type = 'text';
+        input.id = 'createColumn' + i;
+        input.name = 'column' + i;
+        input.required = true;
+
+
+        // Agregar elementos al contenedor
+        columnInputsContainer.appendChild(label);
+        columnInputsContainer.appendChild(input);
+
+
+        // Agregar elemento select (drop-down) para el tipo de columna
+        var typeLabel = document.createElement('label');
+        typeLabel.textContent = 'Tipo' + i + ':';
+
+
+        var select = document.createElement('select');
+        select.id = 'columnType' + i;
+        select.name = 'columnType' + i;
+        select.required = true;
+
+
+        // Opciones para el drop-down (puedes agregar más según tus necesidades)
+        var optionInt = document.createElement('option');
+        optionInt.value = 'INT';
+        optionInt.textContent = 'INT';
+
+
+        var optionVarchar = document.createElement('option');
+        optionVarchar.value = 'VARCHAR(255)';
+        optionVarchar.textContent = 'VARCHAR(255)';
+
+
+        var optionText = document.createElement('option');
+        optionText.value = 'TEXT';
+        optionText.textContent = 'TEXT';
+
+
+        // Agregar opciones al select
+        select.appendChild(optionInt);
+        select.appendChild(optionVarchar);
+        select.appendChild(optionText);
+
+
+        // Agregar elementos de tipo al contenedor
+        columnInputsContainer.appendChild(typeLabel);
+        columnInputsContainer.appendChild(select);
+    }
+}
+
+
+async function crearTabla() {
+    const tableName = document.getElementById('createTableName').value;
+    const numColumnas = document.getElementById('numeroColumnas').value;
+
+
+    // Crear un array para almacenar las columnas
+    const columns = [];
+
+
+    for (let i = 1; i <= numColumnas; i++) {
+        const columnName = document.getElementById('createColumn' + i).value;
+        const columnType = document.getElementById('columnType' + i).value;
+        const column = {
+            name: columnName,
+            type: columnType
+        };
+        columns.push(column);
+    }
+
+
+    // Crear el objeto de datos a enviar al servidor
+    const requestData = {
+        tableName: tableName,
+        columns: columns
+    };
+
+
+    try {
+        // Realizar una solicitud AJAX para crear la tabla en el servidor
+        let result = await fetch('/createTable', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(requestData)
+        });
+
+
+        if (result.ok) {
+            // La solicitud fue exitosa
+            console.log('Tabla creada con éxito');
+            document.getElementById('feedbackMessage').textContent = 'Tabla creada con éxito';
+            // Puedes realizar acciones adicionales aquí si es necesario
+        } else {
+            // La solicitud falló
+            console.error('Error al crear la tabla:', result.status, result.statusText);
+            document.getElementById('feedbackMessage').textContent = 'Error al crear la tabla';
+            // Puedes mostrar un mensaje de error al usuario o realizar acciones adicionales
+        }
+    } catch (error) {
+        console.error('Error al procesar la solicitud:', error);
+        document.getElementById('feedbackMessage').textContent = 'Error al procesar la solicitud';
+        // Puedes mostrar un mensaje de error al usuario o realizar acciones adicionales
+    }
+}
+
+
+async function modificarTabla() {
+    const tableName = document.getElementById('nameModifyTable').value;
+    const columnName = document.getElementById('columnName').value;
+    const columnType = document.getElementById('columnType').value;
+     // Assuming you have an input field for column type
+     const operationSelect = document.getElementById('operation').value;
+    console.log("Table name --->" + tableName);
+    console.log("Column name --->" + columnName);
+    console.log("Column type --->" + columnType);
+
+
+ 
+      const request = {
+        operationSelect:operationSelect,
+        tableName: tableName,
+        columnName: columnName,
+        columnType: columnType // Include columnType in the request
+    };
+
+
+    try {
+        // Realizar una solicitud AJAX para modificar datos en el servidor
+        let result = await fetch('/updateTable', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(request)
+        });
+
+
+        if (result.ok) {
+            // La solicitud fue exitosa
+            console.log('Datos modificados con éxito');
+            // Puedes realizar acciones adicionales aquí si es necesario // Recargar la tabla después de la modificación
+        } else {
+            // La solicitud falló
+            console.error('Error al modificar datos:', result.status, result.statusText);
+            // Puedes mostrar un mensaje de error al usuario o realizar acciones adicionales
+        }
+    } catch (error) {
+        console.error('Error al procesar la solicitud:', error);
+        // Puedes mostrar un mensaje de error al usuario o realizar acciones adicionales
+    }
+}
+function toggleColumnTypeInput() {
+    const operationSelect = document.getElementById('operation');
+    const columnTypeContainer = document.getElementById('columnTypeContainer');
+
+
+    if (operationSelect.value === 'add') {
+        columnTypeContainer.style.display = 'block';
+    } else {
+        columnTypeContainer.style.display = 'none';
+    }
+}
+async function eliminarTabla(){
+    const tableName=document.getElementById('deleteTablebyName').value;
+    console.log("consola --->" + tableName);
+
+
+    const rq = {
+        tableName:tableName
+    }
+      try {
+        // Realizar una solicitud AJAX para modificar datos en el servidor
+        let result = await fetch('/deleteTable', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(rq)
+        });
+
+
+        if (result.ok) {
+            // La solicitud fue exitosa
+            console.log('Datos modificados con éxito');
+            document.getElementById('feedbackMessageDeleteTable').textContent = 'Tabla borrada con éxito';
+            // Puedes realizar acciones adicionales aquí si es necesario // Recargar la tabla después de la modificación
+        } else {
+            // La solicitud falló
+            console.error('Error al modificar datos:', result.status, result.statusText);
+            document.getElementById('feedbackMessageDeleteTable').textContent = 'Error al crear la tabla';
+            // Puedes mostrar un mensaje de error al usuario o realizar acciones adicionales
+        }
+    } catch (error) {
+        console.error('Error al procesar la solicitud:', error);
+        // Puedes mostrar un mensaje de error al usuario o realizar acciones adicionales
+    }
+
+
+
+
 }
